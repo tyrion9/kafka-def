0c820ba1df7ccc854550db3fe690477606979718
tyrion9 <hoaipn@gmail.com>
2018-05-06 21:44:09 +0700

    first commit

---
+++ src/main/java/kafkadef/chapter3/KafkaProducerTest.java
@@ -0,0 +1,32 @@
+package kafkadef.chapter3;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ * Hello world!
+ *
+ */
+public class KafkaProducerTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		Properties kafkaProps = new Properties();
+		kafkaProps.put("bootstrap.servers", "35.197.141.20:9092");
+		kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		KafkaProducer producer = new KafkaProducer<String, String>(kafkaProps);
+
+		ProducerRecord<String, String> record = new ProducerRecord<>("test2", "Precision Products France");
+		try {
+			System.out.println("sending");
+			producer.send(record).get();
+			System.out.println("done");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
